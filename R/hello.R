cb7d0f05089da8e8c754e54adc51f1085fd8483c
Liz <lizwinters@gmail.com>
2018-07-03 15:45:32 -0700

    bioinformatics textbook solutions in R

---
+++ R/hello.R
@@ -0,0 +1,36 @@
+PatternCount <- function(text, pattern){
+  count <- 0
+  for (i in length(text) - length(pattern)){
+    print(text[i : i + length(pattern))
+    #if (text[i : i+length(pattern)] == pattern){
+    #  count <- count + 1
+    }
+    i <- i + 1
+  }
+  print(count)
+}
+
+PatternCount("GCGCG", "GCG")
+
+PatternCount2 <- function(text, pattern){
+  #print(pattern)
+  pattern_count <- 0
+  pattern_length <- nchar(pattern)
+  look_length <- nchar(text) - nchar(pattern)
+  for (i in 1:look_length) {
+    #print(c("i = ", i))
+    end_pos <- i + pattern_length
+    #print(end_pos)
+    test_text <- substr(text, i, end_pos-1)
+    #print(test_text)
+    if (test_text == pattern){
+      pattern_count <- pattern_count + 1
+    }
+  }
+  print(pattern_count)
+}
+
+PatternCount2("GCGCGGGATATATTA", "GCG")
+
+text <- "GCGCGGGATATATTA"
+substr(text, 1, 5)
