2848b7dad12d65559b5d50c2ae6a5913336f57db
Liz <lizwinters@gmail.com>
2018-07-12 15:59:42 -0700

    working on fixing reverse primer maker

--- primer_designer.R
+++ primer_designer.R
@@ -1,3 +1,25 @@
+#returns complement of given base
+complement <- function(nuc){
+  nuc <- toupper(nuc)
+
+  if (nuc == "A"){
+    return("T")
+  }
+  if (nuc == "T"){
+    return("A")
+  }
+  if (nuc == "G"){
+    return("C")
+  }
+  if (nuc == "C"){
+    return("G")
+  }
+  else {
+    return("Invalid base. Enter A, G, C, or T.")
+  }
+}
+
+#designs forward primer, and adjusts length based on melting temperature
 forward_primer_design <- function(gene, primer_length, low_melt_temp, high_melt_temp){
  gene_length <- nchar(gene)
 
@@ -58,6 +80,8 @@ forward_primer_design <- function(gene, primer_length, low_melt_temp, high_melt_
    next_place <- next_place + 1
  }
 
+ return(forward_primer)
+ return(for_melt_temp)
  print(c("forward: ", forward_primer))
  print(nchar(forward_primer))
  print(for_melt_temp)
@@ -71,93 +95,19 @@ nchar("AAAGAGAGAGAGAGAGAG")
 gene <- "AAAGAGAGAGAGAGAGAGACCCCCCACACACACACCACATATTATAGA"
 paste(gene, "t", sep = "")
 
-
+#designs forward primer, and adjusts length based on melting temperature
 reverse_primer_design <- function(gene, primer_length, low_melt_temp, high_melt_temp){
-  gene_length <- nchar(gene)
 
-  reverse_nucs <- substr(gene, nchar(gene) - primer_length, nchar(gene))
-  reverse_primer <- ("")
+  reversed_gene <- paste(rev(substring(gene,1:nchar(gene),1:nchar(gene))),collapse="")
+  proto_reverse_primer <- forward_primer_design(reversed_gene, primer_length, low_melt_temp, high_melt_temp)
 
-  for (i in 1:nchar(reverse_nucs)) {
-    if (substr(reverse_nucs, i, i) == "A" | substr(reverse_nucs, i, i) == "a"){
-      reverse_primer <- paste0(reverse_primer, "T")
-    }
-    if (substr(reverse_nucs, i, i) == "T" | substr(reverse_nucs, i, i) == "t"){
-      reverse_primer <- paste0(reverse_primer, "A")
-    }
-    if (substr(reverse_nucs, i, i) == "G" | substr(reverse_nucs, i, i) == "g"){
-      reverse_primer <- paste0(reverse_primer, "C")
-    }
-    if (substr(reverse_nucs, i, i) == "C" | substr(reverse_nucs, i, i) == "c"){
-      reverse_primer <- paste0(reverse_primer, "G")
-    }
-  }
+  correct_reverse_primer <- paste(rev(substring(proto_reverse_primer,1:nchar(proto_reverse_primer),1:nchar(proto_reverse_primer))),collapse="")
 
-  rev_melt_temp <- 0
-
-  for (i in 1:nchar(reverse_primer)) {
-    if (substr(reverse_primer, i, i) == "A" | substr(reverse_primer, i, i) == "a"){
-      rev_melt_temp <- rev_melt_temp + 2
-    }
-    if (substr(reverse_primer, i, i) == "T" | substr(reverse_primer, i, i) == "t"){
-      rev_melt_temp <- rev_melt_temp + 2
-    }
-    if (substr(reverse_primer, i, i) == "G" | substr(reverse_primer, i, i) == "g"){
-      rev_melt_temp <- rev_melt_temp + 4
-    }
-    if (substr(reverse_primer, i, i) == "C" | substr(reverse_primer, i, i) == "c"){
-      rev_melt_temp <- rev_melt_temp + 4
-    }
-  }
-
-  next_place <- nchar(reverse_nucs) - primer_length + 1
-
-  while (rev_melt_temp < low_melt_temp){
-    if (substr(gene, next_place, next_place) == "A" | substr(gene, next_place, next_place) == "a"){
-      rev_melt_temp <- rev_melt_temp + 2
-      reverse_primer <- paste0(reverse_primer, "T")
-    }
-    if (substr(gene, next_place, next_place) == "T" | substr(gene, next_place, next_place) == "t"){
-      rev_melt_temp <- rev_melt_temp + 2
-      reverse_primer <- paste0(reverse_primer, "A")
-    }
-    if (substr(gene, next_place, next_place) == "G" | substr(gene, next_place, next_place) == "g"){
-      rev_melt_temp <- rev_melt_temp + 4
-      reverse_primer <- paste0(reverse_primer, "C")
-    }
-    if (substr(gene, next_place, next_place) == "C" | substr(gene, next_place, next_place) == "c"){
-      rev_melt_temp <- rev_melt_temp + 4
-      reverse_primer <- paste0(reverse_primer, "G")
-    }
-    next_place <- next_place + 1
-  }
-
-  reverse_primer <- paste(rev(substring(reverse_primer,1:nchar(reverse_primer),1:nchar(reverse_primer))),collapse="")
-
-  print(c("reverse: ", reverse_primer))
-  print(nchar(reverse_primer))
+  print(c("reverse: ", correct_reverse_primer))
+  print(nchar(correct_reverse_primer))
   print(rev_melt_temp)
 }
 
 reverse_primer_design("ttggttaagataagaacccatgtatatataaagggcaaggttcaaggtgtgtactttagacagaatatgcgtaatatagcaaggaagtacaatgtaaacggatgggttaagaaccttaaggatggaagagtagaagctgtacttgaaggtgatgaggatgctgtacatcaagtcatagagtggtgccatataggtcctgctggtgctagggttgatgacgttgatgttgtttatgaagagtacaagggtgagtttaactcatttgatataatatattaa", 18, 55, 60)
 
-#returns complement of given base
-complement <- function(nuc){
-  nuc <- toupper(nuc)
 
-  if (nuc == "A"){
-    return("T")
-  }
-  if (nuc == "T"){
-    return("A")
-  }
-  if (nuc == "G"){
-    return("C")
-  }
-  if (nuc == "C"){
-    return("G")
-  }
-  else {
-    return("Invalid base. Enter A, G, C, or T.")
-  }
-}
